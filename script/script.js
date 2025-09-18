c5fda814b3d985cbf2bc2bc1b6a31ab303c30483
Avlis <leo.silva@etu.unicaen.fr>
2025-09-18 14:01:48 +0200

    ajout base du projet (majorite html)

--- script/script.js
+++ script/script.js
@@ -2,3 +2,7 @@ var canvas = document.querySelector("canvas");
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "green";
 ctx.fillRect(10, 10, 100, 100);
+
+let reset = document.getElementById('resetPartie');
+
+reset.addEventListener
