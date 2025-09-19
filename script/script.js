c7ce7ddf207b49081f2f2a97988d53ad7742725f
Léo <leo.silva@etu.unicaen.fr>
2025-09-19 23:40:25 +0200

    essai reglement probleme actualisation page

--- script/script.js
+++ script/script.js
@@ -1,13 +1,17 @@
-const canvas = document.getElementById("pong");
-var ctx = canvas.getContext("2d");
-ctx.fillStyle = "green";
-ctx.fillRect(50, 50, 150, 100);
-
+window.onload = function() {
+    const canvas = document.getElementById("pong");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "green";
+    ctx.fillRect(50, 50, 150, 100);
+};
 
 
 let reset = document.getElementById('resetPartie');
 
-reset.addEventListener('click', () => {
+reset.addEventListener('click', resetGame);
+
+function resetGame() {
+    
     window.location.reload();
-});
+};
 
